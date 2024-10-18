c9b795414c966a15036473d3ddf133a49d345551
kristof.janssens@mirabilis.be <kristof.janssens@mirabilis.be>
2024-10-18 20:37:54 +0200

    Update

--- misc/header.php
+++ misc/header.php
@@ -9,5 +9,5 @@
         <meta name="description" content="A privacy respecting meta search engine."/>
         <meta name="referrer" content="no-referrer"/>
         <link rel="stylesheet" type="text/css" href="static/css/styles.css"/>
-        <link rel="stylesheet"  type="text/css" href="google.css">
+        <link rel="stylesheet" type="text/css" href="static/css/google.css">
         <link title="<?php printtext("page_title"); ?>" type="application/opensearchdescription+xml" href="opensearch.xml?method=POST" rel="search"/>
