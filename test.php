639723bfdcdda44e566548ddcfc977a49e1c3b29
kristof.janssens@mirabilis.be <kristof.janssens@mirabilis.be>
2024-10-19 20:44:56 +0200

    Update

--- test.php
+++ test.php
@@ -24,10 +24,10 @@ function getGoogleAutocompleteSuggestions($query) {
     }
 
     $decodedString = html_entity_decode($response);
-    $convertedString = mb_convert_encoding($decodedString, 'UTF-8', 'UTF-8');
+    //$convertedString = mb_convert_encoding($decodedString, 'UTF-8', 'UTF-8');
 
     // Step 2: Decode JSON
-    $array = json_decode($convertedString, true);
+    $array = json_decode($decodedString, true);
 
     // Check for errors
     if (json_last_error() !== JSON_ERROR_NONE) {
