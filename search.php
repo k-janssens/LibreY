1bf2bb6d31b6d2e4315c0a53a974918485d52e4f
kristof.janssens@mirabilis.be <kristof.janssens@mirabilis.be>
2024-10-19 19:16:10 +0200

    Update

--- search.php
+++ search.php
@@ -7,9 +7,8 @@
     $opts = load_opts();
 
     $time_options = ['Any time', 'Past 24 hours', 'Past week', 'Past month', 'Past year'];
-   // $selected_time_option = isset($_REQUEST['time_period']) ? $_REQUEST['time_period'] : 'Any time';
+    $selected_time_option = isset($_REQUEST['time_period']) ? $_REQUEST['time_period'] : 'Any time';
     
-    <form method=
     function print_page_buttons($type, $query, $page) {
         if ($type > 1)
             return;
@@ -84,7 +83,13 @@
                         echo "<a " . (($category_index == $opts->type) ? "class=\"active\" " : "") . "href=\"./search.php?q=" . urlencode($opts->query) . "&p=0&t=" . $category_index . "\"><img src=\"static/images/" . $category . "_result.png\" alt=\"" . $category . " result\" />" . TEXTS["category_$category"]  . "</a>";
                     }
                 ?>
-                
+                <select name="time_period" id="time_period" onchange="this.form.submit()>
+                <?php foreach ($time_options as $time_option): ?>
+                    <option value="<?= $time_option ?>" <?= ($selected_time_option == $time_option) ? 'selected' : '' ?>>
+                        <?= $time_option ?>
+                    </option>
+                <?php endforeach; ?>
+                </select>
             </div>
         </form>
 
