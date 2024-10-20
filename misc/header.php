9c56ded9f71a6f7543043db2d0b125302dbd79fa
kristof.janssens@mirabilis.be <kristof.janssens@mirabilis.be>
2024-10-20 10:00:50 +0200

    Update

--- misc/header.php
+++ misc/header.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" type="text/css" href="static/css/google.css">
         <link title="<?php printtext("page_title"); ?>" type="application/opensearchdescription+xml" href="opensearch.xml?method=POST" rel="search"/>
         <script src="static/css/jquery-3.6.0.min.js"></script>
-        <!--link rel="stylesheet" href="static/css/font-awesome.min.css"-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
 
         
